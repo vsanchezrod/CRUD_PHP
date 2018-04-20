d91bc740d7bfd5bb47045dcbe4c1a2aebcae8af3
vsanchezrod <v.sanchez.rod@gmail.com>
2018-04-20 13:24:31 +0200

    Añadiendo botones

--- cabecera.php
+++ cabecera.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8">
 	<title>UF3 PAC1</title>
 	<link rel="stylesheet" href="css/estilos.css">
+	<script type="text/javascript" src="js/javascript.js"></script>
 		
 </head>
 <body>
@@ -13,8 +14,9 @@
 		
 		<h1 class="titulo">PHP - MYSQL </h1>
 		<div id="menu">
-			
 			<ul>
+				<!--Se crea un nuevo botón para acceder al LOGIN del usuario-->
+				<button onclick="abrirVentana()">LOGIN</button>
 				<li><a href="index.php">INICIO</a></li>
 				<li><a href="formularioUsuario.php?opcion=crear">CREAR USUARIO</a></li>
 				<li><a href="formularioProvincias.php">CREAR PROVINCIA</a></li>
---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+function abrirVentana(){
+
+	var nuevaVentana = window.open("login.php", "", "width=500, height=500, left=500px, top=300px, rezisable=no");
+}
+
+
