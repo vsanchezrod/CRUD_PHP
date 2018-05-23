b94035fe2bd57864bca8deeda3e2b9356194b510
vsanchezrod <v.sanchez.rod@gmail.com>
2018-05-23 17:06:41 +0200

    Añadiendo noticias deportivas

---
+++ noticias.php
@@ -0,0 +1,46 @@
+<!-- Se crea la cabecera de la página en php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>UF3 PAC1</title>
+	<link rel="stylesheet" href="css/estilos.css">
+	<script type="text/javascript" src="js/javascript.js"></script>
+		
+</head>
+<body>
+
+    <h2>NOTICIAS DEPORTIVAS!!!</h2>
+
+    <div id="cajaNoticias">
+
+        <div class="noticia">
+            <h3>La FIA vuelve a mediar en la guerra Mercedes-Ferrari por la quema de aceite</h3>
+            <img src="images/noticia1.jpg" alt="Fórmula 1">
+            <p>La FIA sigue aclarando cuestiones en relación a la quema de aceite. Nuevamente realiza esta aclaración a consecuencia de una petición de Mercedes, que en otro aguijonazo a Ferrari, le solicitó a la FIA que dejase claro si el aceite utilizado en el turbo, tiene que cumplir la misma normativa que en el resto de elementos del motor (ICE y V6) o si por el contrario el turbocompresor era una pieza que se podía considerar por separado y ajena por tanto a esas limitaciones.</p>
+        </div>
+
+        <div class="noticia">
+            <h3>Márquez, la historia es suya</h3>
+            <img src="images/noticia2.jpg" alt="Moto GP">
+            <p>Marc Márquez está destinado a hacer historia en el Mundial de motociclismo. De hecho, la realiza a pasos agigantados y con prisa. Su precocidad es exagerada, lo que aventura que su techo, a nivel de los libros sagrados del motociclismo, sólo puede estar en lo más alto.</p>
+        
+        </div>
+        
+        <div class="noticia">
+            <h3>El Dakar 2019 se disputará en Perú y tendrá sólo 10 etapas</h3>
+            <img src="images/noticia3.jpg" alt="Rally Dakar 2018">
+            <p>Pese a las dificultades que incluso obligaron a posponer el anuncio oficial, el Dakar 2019 ya tiene recorrido. La próxima edición del raid sudamericano se disputará íntegramente en Perú, después de que las negociaciones con los otros países candidatos (especialmente Ecuador y Bolivia, pero también Chile y Argentina) no hayan fructificado.</p>
+        
+        </div>
+        
+        <div class="noticia">
+            <h3>La estación sueca de Vilsta eleva diez metros su cota con restos de una obra de Amazon</h3>
+            <img src="images/noticia4.jpg" alt="Deportes de invierno">
+            <p>Vilsta es una estación de esquí situada a una hora de coche al oeste de Estocolmo. Pertenece al municipio de Eskilstuna y fundamentalmente está dedicada al nórdico, aunque tiene cuatro pistas de alpino. La aspiración del club local, Tunafors Ski Club, era la de ganar diez metros de altura y treinta de longitud, con lo que en la primavera del año pasado se puso en marcha el proyecto inicial.</p>
+        </div>
+
+    </div>
+
+</body>
+</html>
